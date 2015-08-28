23e7630a5939106b74e4357db30d6c62d7acf647
Jolie <joliebette@yahoo.com>
2015-08-28 18:59:08 +0000

    final

--- application_controller.rb
+++ application_controller.rb
@@ -1,11 +1,11 @@
 require 'bundler'
 Bundler.require
-require './config/environment'
+#require './config/environment'
 require_relative 'models/model2.rb'
 
 class MyApp < Sinatra::Base
 
-  set :database, {adapter: "sqlite3", database: "db.sqlite3"}
+ # set :database, {adapter: "sqlite3", database: "db.sqlite3"}
 
   get '/' do
     erb :index
